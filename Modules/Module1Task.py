c8cbaa5f8d0bf5fddc519edad4cc657a9b0a72bf
nhcha6 <nhcha6@student.monash.edu>
2020-08-22 14:59:03 +1000

    added comments

--- Modules/Module1Task.py
+++ Modules/Module1Task.py
@@ -17,7 +17,7 @@ led_pin_3 = 15
 btn_pin_1 = 16
 btn_pin_2 = 18
 
-# button press bools
+# initialise button edge bools to check if a button has been pressed
 button_1_edge = False
 button_2_edge = False
 
@@ -35,6 +35,7 @@ GPIO.setup(btn_pin_1, GPIO.IN)
 GPIO.setup(btn_pin_2, GPIO.IN)
 
 
+# thread takes delay time, and the pin numbers of the outer LEDs as input
 class thread(threading.Thread):
     def __init__(self, delay, led_pin_1, led_pin_3):
         threading.Thread.__init__(self)
@@ -46,18 +47,18 @@ class thread(threading.Thread):
         # target function of the thread class
         try:
             while True:
-                GPIO.output(self.led_pin_1, GPIO.HIGH) # Turn LED1 on
-                time.sleep(self.delay) # Delay for 1 second
-                GPIO.output(self.led_pin_1, GPIO.LOW) # Turn LED1 off
-                time.sleep(self.delay) # Delay for 1 second
+                GPIO.output(self.led_pin_1, GPIO.HIGH) # Turn outer LED on
+                time.sleep(self.delay) #delay
+                GPIO.output(self.led_pin_1, GPIO.LOW) # Turn outer LED off
+                time.sleep(self.delay) # delay
                 GPIO.output(13, GPIO.HIGH) # Turn LED2 on
-                time.sleep(self.delay) # Delay for 1 second
+                time.sleep(self.delay) # delay
                 GPIO.output(13, GPIO.LOW) # Turn LED2 off
-                time.sleep(self.delay) # Delay for 1 second
-                GPIO.output(self.led_pin_3, GPIO.HIGH) # Turn LED3 on
-                time.sleep(self.delay) # Delay for 1 second
-                GPIO.output(self.led_pin_3, GPIO.LOW) # Turn LED3 off
-                time.sleep(self.delay) # Delay for 1 second
+                time.sleep(self.delay) # delay
+                GPIO.output(self.led_pin_3, GPIO.HIGH) # Turn outer LED on
+                time.sleep(self.delay) # delay
+                GPIO.output(self.led_pin_3, GPIO.LOW) # Turn outer LED off
+                time.sleep(self.delay) # delay
 
 
         finally:
@@ -83,17 +84,20 @@ class thread(threading.Thread):
 def signal_handler(sig, frame):
     GPIO.cleanup()
     sys.exit(0)
-    
+
+# interrupt function of button 1 changes global boolean
 def button_callback_1(channel):
     global button_1_edge
     button_1_edge = True
 
+# interrupt function of button 2 changes global boolean
 def button_callback_2(channel):
     global button_2_edge
     button_2_edge = True
 
 if __name__ == '__main__':
 
+    # checked for rising edge of either button
     GPIO.add_event_detect(btn_pin_1, GPIO.RISING, callback = button_callback_1, bouncetime=50)
     GPIO.add_event_detect(btn_pin_2, GPIO.RISING, callback = button_callback_2, bouncetime=50)
     
@@ -104,30 +108,45 @@ if __name__ == '__main__':
     blinking_thread.start()
 
     while True:
+
+        # if rising edge of button 1 was detected
         if button_1_edge:
+            # exit from the run block of the old thread
             blinking_thread.raise_exception()
+            # turn all LEDs off
             GPIO.output(11, GPIO.LOW)
             GPIO.output(13, GPIO.LOW)
             GPIO.output(15, GPIO.LOW)
             # change delay from 0.5 to 2 or 2 to 0.5
             delay=1/delay
+            # start a new thread with new delay
             blinking_thread_temp = thread(delay,led_pin_1,led_pin_3)
             blinking_thread_temp.start()
+            # terminate thread safely with join()
             blinking_thread.join()
+            # set boolean back to false
             button_1_edge = False
+            # rename new thread as main thread
             blinking_thread = blinking_thread_temp
-            
+
+         # if rising edge of button 2 was detected           
         if button_2_edge:
+            # exit from the run block of the old thread
             blinking_thread.raise_exception()
+            # turn all LEDs off
             GPIO.output(11, GPIO.LOW)
             GPIO.output(13, GPIO.LOW)
             GPIO.output(15, GPIO.LOW)
             # switch button pin numbers to change sequence order
             led_pin_1, led_pin_3 = led_pin_3, led_pin_1
+            # start a new thread with new pin numbers
             blinking_thread_temp = thread(delay,led_pin_1,led_pin_3)
             blinking_thread_temp.start()
+            # terminate thread safely with join()
             blinking_thread.join()
+            # set boolean back to false
             button_2_edge = False
+            # rename new thread as main thread
             blinking_thread = blinking_thread_temp
 
 
