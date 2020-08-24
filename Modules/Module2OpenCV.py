c4e8b626b61c5a778bbaf7006ddcc9b91a3c6bde
nhcha6 <nhcha6@student.monash.edu>
2020-08-24 16:19:52 +1000

    added bluring/pixelation function

--- Modules/Module2OpenCV.py
+++ Modules/Module2OpenCV.py
@@ -3,6 +3,7 @@ from picamera.array import PiRGBArray
 import numpy as np
 import cv2
 import time
+import bluring
 
 # function for displaying video of raspberry pi
 def continuousCapture():
@@ -33,6 +34,49 @@ def continuousCapture():
     # When everything done, release the capture
     # cv2.destroyAllWindows()
 
+def blurImage():
+    # initialise object
+    camera = PiCamera()
+    # configure camera setting
+    camera.resolution = (640, 480)
+    camera.framerate = 32
+    # initialise the picture arrage with the corresponding size
+    rawCapture = PiRGBArray(camera, size=(640, 480))
+    blur = 0
+    pix = 0
+    # capture frames from the camera
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 
-continuousCapture()
+        # grab the raw numpy array representing the image, then initialise the timestamp and occiped/unoccupied text
+        image = frame.array
+        blurseimage = image;
+        if blur == 1:
+            blurseimage = cv2.GaussianBlur(image, (51, 51), 0)
+
+        if pix == 1:
+            blurseimage = bluring.img_pixelate(image, 100)
+
+        # Display the resulting frame
+        cv2.imshow("PP", blurseimage)
+        # clear the stream in preparation for the next frame
+        rawCapture.truncate(0)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('b'):
+            # toggle the blur output
+            blur = 1 - blur
+        if key == ord('p'):
+            # toggle the blur output
+            pix = 1 - pix
+
+        if key == ord('q'):
+            break
+
+    # When everything done, release the capture
+    # cv2.destroyAllWindows()
+
+
+#continuousCapture()
+
+blurImage()
 
