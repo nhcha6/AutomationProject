8ef9230290ef2f1a64b5460d0b0e9c31a7690fa8
Nic <nhcha6@student.monash.edu>
2020-09-21 01:31:29 +0100

    images send from RPI to computer. Computer sends flag to end program to RPI

--- ProjectTests/image_server.py
+++ ProjectTests/image_server.py
@@ -6,31 +6,45 @@ import numpy as np
 
 # Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
 # all interfaces)
-server_socket = socket.socket()
-server_socket.bind(('0.0.0.0', 8000))
-server_socket.listen(0)
+image_server_socket = socket.socket()
+image_server_socket.bind(('0.0.0.0', 8000))
+image_server_socket.listen(0)
+
+# start new socket to send data back to the client
+result_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+result_server_socket.bind(('0.0.0.0', 8081))
+result_server_socket.listen(0)
 
 # Accept a single connection and make a file-like object out of it
-connection = server_socket.accept()[0].makefile('rb')
+image_connection = image_server_socket.accept()[0].makefile('rb')
+result_connection = result_server_socket.accept()[0]
 try:
     while True:
         # Read the length of the image as a 32-bit unsigned int. If the
         # length is zero, quit the loop
-        image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+        image_len = struct.unpack('<L', image_connection.read(struct.calcsize('<L')))[0]
         if not image_len:
             break
         # Construct a stream to hold the image data and read the image
         # data from the connection
         image_stream = io.BytesIO()
-        image_stream.write(connection.read(image_len))
+        image_stream.write(image_connection.read(image_len))
         # Rewind the stream, open it as an image with PIL and do some
         # processing on it
         image_stream.seek(0)
         file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
         img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
         cv2.imshow("Image", img)
-        cv2.waitKey(1)
+
+        message = 'nope'
+        result_connection.send(message.encode())
+        if cv2.waitKey(1) & 0xFF == ord('s'):
+            print('yep')
+            message = 'close'
+            result_connection.send(message.encode())
 
 finally:
-    connection.close()
-    server_socket.close()
+    image_connection.close()
+    image_server_socket.close()
+    result_connection.close()
+    result_server_socket.close()
--- ProjectTests/rpi_client.py
+++ ProjectTests/rpi_client.py
@@ -3,14 +3,62 @@ import socket
 import struct
 import time
 import picamera
+import threading
+import ctypes
+
+# thread takes delay time, and the pin numbers of the outer LEDs as input
+class thread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        # target function of the thread class
+        try:
+            result_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result_client_socket.connect(('Nicolass-MacBook-Air.local', 8081))
+            while True:
+                #time.sleep(1)
+                result = result_client_socket.recv(4096)
+                close_message = 'close'
+                if result == close_message.encode():
+                    print(result.decode())
+                    global closeFlag
+                    closeFlag = True
+
+        finally:
+            result_client_socket.close()
+            print('ended')
+
+    def get_id(self):
+        # returns id of the respective thread
+        if hasattr(self, '_thread_id'):
+            return self._thread_id
+        for id, thread in threading._active.items():
+            if thread is self:
+                return id
+
+    def raise_exception(self):
+        thread_id = self.get_id()
+        # stop the thread when the exception received
+        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id,
+                                                         ctypes.py_object(SystemExit))
+        if res > 1:
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0)
+            print('Exception raise failure')
+
+result_thread = thread()
+result_thread.start()
 
 # Connect a client socket to my_server:8000 (change my_server to the
 # hostname of your server)
-client_socket = socket.socket()
-client_socket.connect(('Nicolass-MacBook-Air.local', 8000))
+image_client_socket = socket.socket()
+image_client_socket.connect(('Nicolass-MacBook-Air.local', 8000))
 
 # Make a file-like object out of the connection
-connection = client_socket.makefile('wb')
+image_connection = image_client_socket.makefile('wb')
+
+# flag for when close message recieved
+closeFlag = False
 try:
     camera = picamera.PiCamera()
     camera.resolution = (640, 480)
@@ -22,24 +70,25 @@ try:
     # temporarily (we could write it directly to connection but in this
     # case we want to find out the size of each capture first to keep
     # our protocol simple)
-    start = time.time()
     stream = io.BytesIO()
     for foo in camera.capture_continuous(stream, 'jpeg'):
         # Write the length of the capture to the stream and flush to
         # ensure it actually gets sent
-        connection.write(struct.pack('<L', stream.tell()))
-        connection.flush()
+        image_connection.write(struct.pack('<L', stream.tell()))
+        image_connection.flush()
         # Rewind the stream and send the image data over the wire
         stream.seek(0)
-        connection.write(stream.read())
+        image_connection.write(stream.read())
         # If we've been capturing for more than 30 seconds, quit
-        if time.time() - start > 30:
+        if closeFlag:
             break
         # Reset the stream for the next capture
         stream.seek(0)
         stream.truncate()
     # Write a length of zero to the stream to signal we're done
-    connection.write(struct.pack('<L', 0))
+    image_connection.write(struct.pack('<L', 0))
 finally:
-    connection.close()
-    client_socket.close()
+    result_thread.raise_exception()
+    result_thread.join()
+    image_connection.close()
+    image_client_socket.close()
