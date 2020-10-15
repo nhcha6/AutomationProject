227f524b726cdf1bcf5c312830552ebce59640c5
nhcha6 <nhcha6@student.monash.edu>
2020-10-15 16:05:56 +1100

    new script for module4 rpi

--- Modules/rpiModule4.py
+++ Modules/rpiModule4.py
@@ -1,26 +1,28 @@
 import numpy as np
 import cv2
 import tensorflow as tf
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+import time
 
+camera = PiCamera()
+# configure camera setting
+camera.resolution = (640, 480)
+camera.framerate = 32
+desire_pos = (320, 240)
+# sleep and update settings
+time.sleep(2)
+camera.awb_mode = 'off'
+camera.awb_gains = 1.3
+# initialise the picture arrage with the corresponding size
+rawCapture = PiRGBArray(camera, size=(640, 480))
 
-cap = cv2.VideoCapture(0)
+# continuously capture frames
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    image = frame.array
+    cv2.imshow("image", image)
 
-# Check if the webcam is opened correctly
-if not cap.isOpened():
-    raise IOError("Cannot open webcam")
-
-while True:
-    ret, frame = cap.read()
-    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-    cv2.imshow('Input', frame)
-
-    frame = cv2.resize(frame, (128, 128))
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    input_image = frame.reshape(-1, 128, 128, 1)
-    input_image = input_image.astype(np.float32)
-    #print(input_image[0])
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('r'):
         # Load the TFLite model and allocate tensors.
         interpreter = tf.lite.Interpreter(model_path="model.tflite")
         interpreter.allocate_tensors()
@@ -42,3 +44,5 @@ while True:
         output_data = interpreter.get_tensor(output_details[0]['index'])
         print(output_data)
 
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
